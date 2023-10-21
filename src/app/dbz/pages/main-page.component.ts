3f04d953b0a0e72830bd76b3096ffcfefaeb27a6
juan18506 <juan185067@gmail.com>
2023-10-20 21:48:02 -0300

    created first service

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
-export class MainPageComponent {
+export class MainPageComponent { 
 
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000,
-  }, {
-    name: 'Goku',
-    power: 9500,
-  }, {
-    name: 'Vegeta',
-    power: 7500,
-  }];
+  constructor(
+    public dbzService: DbzService,
+  ) { }
 
-  onNewCharacter(character: Character): void {
-    this.characters.push(character);
-  }
-
-  onDeletedCharacter(index: number): void {
-    this.characters.splice(index, 1);
-  }
 }
